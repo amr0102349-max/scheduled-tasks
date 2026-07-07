1405b87b82a33d9e9f69478580cb1895bb44517d
amr0102349-max <amr0102349@gmail.com>
2026-07-07 16:53:47 +0300

    Update email handling and birthday notification logic
    Refactor birthday email notification logic and update email credentials.

--- main.py
+++ main.py
@@ -12,27 +12,27 @@ import random
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("amr0102349@gmail.com")
+MY_PASSWORD = os.environ.get("sijabiimtcyxfjdt")
+now = dt.datetime.now()
+today = (now.month , now.day)
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
+# 1. Update the birthdays.csv
 data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+dict_of_birthday ={(data_row["month"] , data_row["day"]) : data_row for (index , data_row) in data.iterrows()}
+name_of_person = dict_of_birthday[today]
+# 2. Check if today matches a birthday in the birthdays.csv
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+if today in dict_of_birthday :
+   letters = f"letter_templates/letter_{random.randint(1,3)}.txt"
+   with open (letters) as letter_file :
+      contents =  letter_file.read()
+      contents = contents.replace("[NAME]" , name_of_person ["name"])
+
+   with smtplib.SMTP("smtp.gmail.com" , 587) as connection :
+       connection.starttls()
+       connection.login(user = MY_EMAIL , password = PASSWORD)
+       connection.sendmail(from_addr= MY_EMAIL  ,
+                           to_addrs= name_of_person ["email"],
+                           msg = f"subject : Happy birthday\n\n {contents}")
+
